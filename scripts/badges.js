11d1e61d7ffa2b19f016f6687c968fa6a71335d8
voltsonic <6464019+voltsonic@users.noreply.github.com>
2019-11-03 13:39:29 -0700

    removed old badge generator

--- scripts/badges.js
+++ scripts/badges.js
@@ -1,16 +1,1 @@
-const fs = require("fs");
-const path = require("path");
-const _ = require("lodash");
-const remark = require("remark");
-const badges = require("@thebespokepixel/badges");
 
-/* …then read the 'badges.readme' stanza from package.json and send the AST into remark etc. */
-badges('readme')
-    .then(badges => {
-        const content = { badges };
-        const template = _.template(fs.readFileSync(path.join(__dirname, 'templates/README.md')));
-        remark().process(template(content))
-            .then(page => {
-                fs.writeFileSync(path.join(__dirname, '..', 'README.md'), page, "utf8");
-            });
-    });
